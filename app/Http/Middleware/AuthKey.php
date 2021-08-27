74215c1edee74f6dae152399bce0471177e29928
bwf1dodolo <82048279+bwf1dodolo@users.noreply.github.com>
2021-08-27 10:43:02 +0530

    Add files via upload

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->header('APP_KEY');
+        if($request->input('APP_KEY') != 'helloatg')
+        {
+            $url = $request->url();
+            if(Str::endsWith($url, 'status'))
+            {
+                return response()->json(['task' => $request->input('task_id'),
+                                     'status' => '0',
+                                    'message' => "Invalid Api Key"
+                ]);
+            }
+            elseif(Str::endsWith($url, 'add'))
+            {
+                return response()->json(['task' => $request->input('task'),
+                                            'status' => '0',
+                                        'message' => "Invalid Apiii Key"
+                    ]);
+            }
+            else
+            {
+                return $next($request);
+            }
+        }
+        return $next($request);
+    }
+}
